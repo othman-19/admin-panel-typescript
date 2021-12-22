f92ad7319dacb489c66d8225509a3a085a1fff82
othmanenaamani@gmail.com-at-971670226036 <othmanenaamani@gmail.com>
2021-12-22 12:31:26 +0100

    change the form component implementation

--- src/components/ login.component.tsx
+++ src/components/ login.component.tsx
@@ -1,83 +1,71 @@
-import { FC, useState} from 'react'
-import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as React from "react";
+import { withFormik, FormikProps } from "formik";
 import * as Yup from "yup";
 
-const Login: FC = () => {
+const InnerForm = (props: OtherProps & FormikProps<FormValues>) => {
+    const {
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        title
+    } = props;
 
-  const [state, setState] = useState({
-    username: "",
-    password: "",
-    loading: false,
-    message: ""
-  })
+    return (
+      <><h1>{title}</h1><form onSubmit={handleSubmit}>
+        <label>Email</label>
+        <input
+          width={50}
+          type="email"
+          name="email"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.email} />
 
-  const validationSchema = () => {
-    return Yup.object().shape({
-      username: Yup.string().required("This field is required!"),
-      password: Yup.string().required("This field is required!"),
-    });
-  }
+        <label>Password</label>
+        <input
+          width={50}
+          type="password"
+          name="password"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.password} />
 
-  const handleLogin = (formValue: { username: string; password: string }) => {
-    const { username, password } = formValue;
-  }
-
-  return (
-    <div className="col-md-12">
-      <div className="card card-container">
-        <img
-          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-          alt="profile-img"
-          className="profile-img-card"
-        />
-
-        <Formik
-          initialValues={initialValues}
-          validationSchema={this.validationSchema}
-          onSubmit={this.handleLogin}
+        <button
+          type="submit"
+          disabled={isSubmitting ||
+            !!(errors.email && touched.email) ||
+            !!(errors.password && touched.password)}
         >
-          <Form>
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <Field name="username" type="text" className="form-control" />
-              <ErrorMessage
-                name="username"
-                component="div"
-                className="alert alert-danger"
-              />
-            </div>
+          Sign In
+        </button>
+      </form></>
 
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <Field name="password" type="password" className="form-control" />
-              <ErrorMessage
-                name="password"
-                component="div"
-                className="alert alert-danger"
-              />
-            </div>
+    );
+};
 
-            <div className="form-group">
-              <button type="submit" className="btn btn-primary btn-block" disabled={loading}>
-                {loading && (
-                  <span className="spinner-border spinner-border-sm"></span>
-                )}
-                <span>Login</span>
-              </button>
-            </div>
+const Login = withFormik<MyFormProps, FormValues>({
+    mapPropsToValues: props => ({
+        email: props.initialEmail || "",
+        password: props.initialPassword || ""
+    }),
 
-            {message && (
-              <div className="form-group">
-                <div className="alert alert-danger" role="alert">
-                  {message}
-                </div>
-              </div>
-            )}
-          </Form>
-        </Formik>
-      </div>
-    </div>
-  );
-}
+    validationSchema: Yup.object().shape({
+        email: Yup.string()
+            .email("Email not valid")
+            .required("Email is required"),
+        password: Yup.string().required("Password is required")
+    }),
+
+    handleSubmit(
+        { email, password }: FormValues,
+        { props, setSubmitting, setErrors }
+    ) {
+        console.log(email, password);
+    }
+})(InnerForm);
 
 export default Login;
