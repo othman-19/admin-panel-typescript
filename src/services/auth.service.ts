a88c45a2bc0efa2a0f5eabcacc406b742831f4bc
othmanenaamani@gmail.com-at-971670226036 <othmanenaamani@gmail.com>
2021-12-23 16:48:46 +0100

    setup the login auth service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,1 +1,24 @@
-export {}
+import axios from "axios";
+import { FormValues } from "../interfaces"
+
+const API_URL = "http://localhost:8080/FakeApi/auth/";
+
+const login = async (values: FormValues) => {
+  const { email, password } = values;
+  try {
+    const response = await axios.post(
+      API_URL + "signin",
+      { email, password }
+    )
+    const { data, data:{accessToken} } = response
+    if (accessToken) {
+      localStorage.setItem("user", JSON.stringify(data));
+    }
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+export { login }
