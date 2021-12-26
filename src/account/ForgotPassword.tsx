d0172b4bf557cae0cbb3a6dbfaad20402a6982f3
othmanenaamani@gmail.com-at-971670226036 <othmanenaamani@gmail.com>
2021-12-26 12:08:33 +0100

    use only Formik with the forgot password component

--- src/account/ForgotPassword.tsx
+++ src/account/ForgotPassword.tsx
@@ -1,94 +1,105 @@
 import React, { useState } from "react";
-import { withFormik, FormikProps, ErrorMessage, Field, Form } from "formik";
+import  { Link } from 'react-router-dom'
+import  { useNavigate } from 'react-router'
+import { Formik, FormikHelpers, ErrorMessage, Field, Form} from "formik";
 import * as Yup from "yup";
+
 // import { forgotPassword } from "../services/auth.service";
 import image from "../assets/moodme-logo.png"
+import { ForgotEmail } from '../Models';
 
-import { ForgotEmail, MyFormProps, OtherProps } from '../Models';
 
-const InnerForm = (props: OtherProps & FormikProps<ForgotEmail>) => {
+const ForgotPassword = () => {
+
   const [message, setMessage] = useState("")
-  const {
-      values,
-      errors,
-      touched,
-      handleChange,
-      handleBlur,
-      handleSubmit,
-      isSubmitting,
-      title
-  } = props;
+  const navigate = useNavigate()
+  
+  const initialValues : ForgotEmail = {
+      email: '',
+  };
+
+  const validationSchema= Yup.object().shape({
+    email: Yup.string()
+      .email("Email not valid")
+      .required("Email is required")
+  });
+
+  const handleSubmit = (
+    { email }: ForgotEmail,
+    { setSubmitting }: FormikHelpers<ForgotEmail>
+  ) => {
+    try {
+      // await forgotPassword({email, password});
+      console.log({email})
+      setMessage("Message from forgot password")
+      navigate("/resetPassword")
+    } catch(error) {
+      setSubmitting(false);
+      console.log(error)
+    }
+  }
 
   return (
-    <div className="col-md-12">
-      <div className="card card-container">
-        <h1>{title}</h1>
-        <img
-          src={image}
-          alt="profile-img"
-          className="profile-img-card"
-        />
-        <Form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <Field
-              className="form-control"
-              type="email"
-              name="email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ errors, touched, isSubmitting}) => (
+        <div className="col-md-12">
+          <div className="card card-container">
+            <h1>Forgot Password</h1>
+            <img
+              src={image}
+              alt="profile-img"
+              className="profile-img-card"
             />
-            <ErrorMessage
-              name="email"
-              component="div"  
-              className="alert alert-danger"
-            />
-          </div>
-          <div className="form-group my-4" >
-            <button
-              type="submit"
-              className="btn btn-primary btn-block"
-              disabled={isSubmitting || (!!errors.email && touched.email)
-              }
-            >
-              {isSubmitting && (
-                <span className="spinner-border spinner-border-sm"></span>
-              )}
-              <span>Get temporary Password</span>
-            </button>
-          </div>
-          {message && (
-            <div className="form-group my-4">
-              <div className="alert alert-danger" role="alert">
-                {message}
+            <Form>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <Field
+                  className="form-control"
+                  type="email"
+                  name="email"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"  
+                  className="alert alert-danger"
+                />
+    
               </div>
-            </div>
-          )}
-        </Form>
-      </div>
-    </div>
-  );
-};
-
-const ForgotPassword = withFormik<MyFormProps, ForgotEmail>({
-    mapPropsToValues: props => ({
-        email: props.initialEmail || ""
-    }),
-
-    validationSchema: Yup.object().shape({
-        email: Yup.string()
-            .email("Email not valid")
-            .required("Email is required")
-    }),
-
-    handleSubmit(
-      { email }: ForgotEmail,
-      { props, setSubmitting, setErrors }
-    ) {
-      // forgotPasword({email});
-      console.log(email);
-    }
-})(InnerForm);
+              <div className="form-group my-4" >
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-block"
+                  disabled={
+                    isSubmitting ||
+                    (!!errors.email && touched.email)
+                  }
+                >
+                  {isSubmitting && (
+                    <span className="spinner-border spinner-border-sm"></span>
+                  )}
+                  <span>Sign In</span>
+                </button>
+              </div>
+              {message && (
+                <div className="form-group my-4">
+                  <div className="alert alert-danger" role="alert">
+                    {message}
+                  </div>
+                </div>
+              )}
+            </Form>
+            <Link to="/login">
+              Forgot Password?
+            </Link>
+          </div>
+        </div>
+      )}
+    </Formik>
+  )
+}
 
 export default ForgotPassword;
