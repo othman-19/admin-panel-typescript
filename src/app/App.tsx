508b5ce294e0dc4e2ca5470a34e691a98dacc34b
othmanenaamani@gmail.com-at-971670226036 <othmanenaamani@gmail.com>
2022-01-02 13:43:34 +0100

    added bootstrap navbar component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,12 @@ import React, { FC } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import AppRouter from './Router';
+import Navbar from '../components/UI/Navbar';
 
 const App: FC = () => {
   return (
     <div className="App">
+      <Navbar/ >
       <AppRouter />
     </div>
   );
