7b92f4363b382005edbaa712aa777ac3498d3276
othmanenaamani@gmail.com-at-971670226036 <othmanenaamani@gmail.com>
2022-01-03 13:32:32 +0100

    added Moodme logo to navbar

---
+++ src/components/UI/MoodmeLogo.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import image from "../../assets/moodme-logo.png"
+
+const MoodmeLogo: FC = () => <img
+  src={image}
+  alt="profile-img"
+  className="small"
+/>
+
+export default MoodmeLogo;
--- src/components/UI/Navbar.tsx
+++ src/components/UI/Navbar.tsx
@@ -1,24 +1,25 @@
 import React, { FC } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import AppRouter from '../../app/Router';
+import MoodmeLogo from './MoodmeLogo';
 
-const Navbar: FC = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary px-4">
-    <a className="navbar-brand" href="#">Navbar</a>
+const Navbar: FC = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-primary px-4">
+    <a className="navbar-brand" href="#">
+      <MoodmeLogo />
+    </a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div className="navbar-nav">
-      <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
-      <a className="nav-item nav-link" href="#">Features</a>
-      <a className="nav-item nav-link" href="#">Pricing</a>
-      <a className="nav-item nav-link disabled" href="#">Disabled</a>
+      <div className="navbar-nav">
+        <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+        <a className="nav-item nav-link" href="#">Features</a>
+        <a className="nav-item nav-link" href="#">Pricing</a>
+        <a className="nav-item nav-link disabled" href="#">Disabled</a>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
   );
-}
 
 export default Navbar;
